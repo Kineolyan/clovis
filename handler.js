cf1724ee177693f483bc9779bba256d5a16842af
Kineolyan <kineolyan@gmail.com>
2019-01-26 20:30:11 +0100

    Initial integration to aws.

---
+++ handler.js
@@ -0,0 +1,17 @@
+function hello(event, context, callback) {
+  console.log(event); // Contains incoming request data (e.g., query params, headers and more)
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      "x-custom-header" : "My Header Value"
+    },
+    body: JSON.stringify({ "message": "Hello World!" })
+  };
+
+  callback(null, response);
+}
+
+module.exports = {
+  hello: hello
+};
