539dc81f6f5f1169c626b1917120c8b62ba51655
Kineolyan <kineolyan@protonmail.com>
2020-04-22 21:25:05 +0200

    Edit pipelines.

--- test/google/tasks.test.js
+++ test/google/tasks.test.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-const {__private__: m} = require('../../../google/tasks');
-=======
 const {__private__: m} = require('../../lib/google/tasks');
->>>>>>> origin/master
 
 describe('#getFrequencyOffset', () => {
   const ONE_DAY = 24 * 3600 * 1000;
