f9031461a27a852012a5a652b7b4ae3cca68d309
Kineolyan <kineolyan@protonmail.com>
2020-04-24 22:26:44 +0200

    Change accessors to the spreadsheet api.

--- src/lib/google/sheets.cljs
+++ src/lib/google/sheets.cljs
@@ -5,16 +5,14 @@
   [auth]
   (gg/google.sheets (clj->js {:version "v4" :auth auth})))
 
+(defn value-api
+  [api]
+  (aget api "spreadsheets" "values"))
+
 (defn read-values
   [api payload callback]
-  (-> api
-      (.-spreadsheets)
-      (.-values)
-      (.get payload callback)))
+  (.get (value-api api) payload callback))
 
 (defn update-values
   [api payload callback]
-  (-> api
-      (.-spreadsheets)
-      (.-values)
-      (.update payload callback)))
+  (.update (value-api api) payload callback))
