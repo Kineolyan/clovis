606c9375b7cf5a90b196a9481f8fb1d5935dec49
Kineolyan <kineolyan@protonmail.com>
2022-03-27 16:00:48 +0200

    Drop unused google methods.

--- src/lib/google/auth.cljs
+++
@@ -1,9 +0,0 @@
-(ns lib.google.auth
-  (:require ["googleapis" :as gg]))
-
-(defn create-service-auth
-  [scopes]
-  (let [auth (.. gg/google -auth)
-        options (clj->js {:scopes scopes})
-        client (.getClient auth options)]
-    (constantly client)))
